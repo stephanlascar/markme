0f9423cba2c79aff8876c181a71e8a00f3180a83
Stéphan Lascar <stephan.lascar@gmail.com>
2014-12-26 12:50:13 +0100

    Un bookmark ne pas pas contenir une description supérieure à 700 caractères

--- forms/bookmark.py
+++ forms/bookmark.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, HiddenField, BooleanField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Length, Optional
 from fields.tag_list_field import TagListField
 
 
@@ -8,7 +8,7 @@ class BookmarkForm(Form):
 
     title = StringField('Titre', validators=[DataRequired()])
     url = StringField('URL', validators=[DataRequired(), URL()])
-    description = TextAreaField('Description')
+    description = TextAreaField('Description', validators=[Optional(), Length(max=700)])
     referrer = HiddenField('Referrer')
     public = BooleanField('Public')
     tags = TagListField('Tags')
