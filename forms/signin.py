1bd769b17c9847596186f832e2f776cd74bb7ded
Stéphan Lascar <stephan.lascar@gmail.com>
2014-12-26 11:41:08 +0100

    Impossibilité de créer un utiliseur en doublon

--- forms/signin.py
+++ forms/signin.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import Form
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, ValidationError
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, EqualTo
+from database import mongo
 
 
 class SigninForm(Form):
@@ -10,3 +12,11 @@ class SigninForm(Form):
     password = PasswordField(u'Mot de passe', validators=[DataRequired()])
     confirm_password = PasswordField(u'Confirmer le mot de passe', validators=[DataRequired(), EqualTo('password', message=u'Les mots de passe doivent correspondre')])
     nickname = StringField(u'Nom d\'utilisateur', validators=[DataRequired()])
+
+    def validate_email(self, field):
+        if mongo.db.users.find_one({'email': field.data}):
+            raise ValidationError(field.gettext(u'Cet email est déjà utilisé.'))
+
+    def validate_nickname(self, field):
+        if mongo.db.users.find_one({'nickname': field.data}):
+            raise ValidationError(field.gettext(u'Ce nom d\'utilisateur est déjà utilisé.'))
