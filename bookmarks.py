b272b77e9a9cb27d70632b5ac862f8f3709edc7e
Stéphan Lascar <stephan.lascar@gmail.com>
2014-12-29 14:12:01 +0100

    Création d'un bookmark depuis la webapp en plus de la bookmarklet

--- bookmarks.py
+++ bookmarks.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 import datetime
 
 from bson import ObjectId, SON
-from flask import Blueprint, render_template, request, flash, current_app
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask.ext.login import login_required, login_user, current_user
 import pymongo
 
@@ -57,12 +58,14 @@ def bookmarks_by_tags(tag):
     bookmarks = mongo.db.bookmarks.find(criteria).sort('date', pymongo.DESCENDING)
     return render_template('bookmarks/public.html', bookmarks=bookmarks, tags=_get_top_tags(criteria), users=_get_most_active_users(), form=form)
 
+
 @bookmarks.route('/my/tags')
 @login_required
 def private_tags():
     criteria = {'user._id': ObjectId(current_user.get_id())}
     tags = mongo.db.bookmarks.find(criteria).distinct('tags')
     return render_template('tags/private.html', my_tags=tags, tags=_get_top_tags(criteria), users=_get_most_active_users())
+
 
 @bookmarks.route('/filter', methods=['POST'])
 def search():
@@ -100,17 +103,35 @@ def bookmarklet():
 def add_bookmark():
     form = BookmarkForm()
     if form.validate_on_submit():
-        mongo.db.bookmarks.save({'title': form.title.data,
-                                 'url': form.url.data,
-                                 'description': form.description.data,
-                                 'referrer': form.referrer.data,
-                                 'tags': form.tags.data,
-                                 'published': datetime.datetime.utcnow(),
-                                 'public': form.public.data,
-                                 'user': {
-                                     '_id': ObjectId(current_user.get_id()),
-                                     'nickname': current_user.nickname,
-                                     'email': current_user.email
-                                 }})
+        _save_bookmark(form)
         return '<script type="text/javascript">window.close();</script>'
     return render_template('bookmarklet.html', form=form)
+
+
+@bookmarks.route('/bookmarks/new', methods=['GET', 'POST'])
+@login_required
+def new_bookmark():
+    form = BookmarkForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        _save_bookmark(form)
+        flash(u'Votre bookmark est correctement enregistré.')
+        return redirect(url_for('.index'))
+
+    criteria = {'user._id': ObjectId(current_user.get_id())}
+    return render_template('bookmarks/new.html', tags=_get_top_tags(criteria), users=_get_most_active_users(), form=form)
+
+
+def _save_bookmark(bookmark_form):
+    mongo.db.bookmarks.save({'title': bookmark_form.title.data,
+                                     'url': bookmark_form.url.data,
+                                     'description': bookmark_form.description.data,
+                                     'referrer': bookmark_form.referrer.data,
+                                     'tags': bookmark_form.tags.data,
+                                     'published': datetime.datetime.utcnow(),
+                                     'public': bookmark_form.public.data,
+                                     'user': {
+                                         '_id': ObjectId(current_user.get_id()),
+                                         'nickname': current_user.nickname,
+                                         'email': current_user.email
+                                     }})
